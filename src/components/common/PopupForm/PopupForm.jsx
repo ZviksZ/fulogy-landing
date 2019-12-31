bfa5ad6b69083adf5f01d415fecc31cca66b7c14
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-12-31 10:26:42 +0300

    popup form 100% + style updates

--- src/components/common/PopupForm/PopupForm.jsx
+++ src/components/common/PopupForm/PopupForm.jsx
@@ -1,59 +1,97 @@
-import {Formik}  from "formik";
-import React     from 'react'
-import s         from './PopupForm.module.scss'
-import * as Yup  from "yup";
-import InputMask from 'react-input-mask';
-import popupImg  from '../../../../public/static/assets/img/popup-form-img.png'
+import {Formik}       from "formik";
+import React          from 'react'
+import {connect}      from "react-redux";
+import {setPopupMode} from "../../../reducers/commonReducer.js";
+import s              from './PopupForm.module.scss'
+import * as Yup       from "yup";
+import InputMask      from 'react-input-mask';
+import popupImg       from '../../../../public/static/assets/img/popup-form-img.png'
+import cn             from 'classnames'
 
 export const popupFormSchema = Yup.object().shape({
    name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
    phone: Yup.string().required('Required'),
 });
 
-export const PopupForm = () => {
+const PopupForm = ({setPopupMode}) => {
    return (
       <div className={s.popupFormContainer} id="popupForm">
          <div className={s.popupFormImg}>
             <img src={popupImg} alt=""/>
          </div>
-         <Formik
-            initialValues={{
-               name: '',
-               phone: '',
-               policy: false
-            }}
-            validationSchema={popupFormSchema}
-            onSubmit={(values, actions) => {
-               alert(values.name)
-            }}
-         >
-            {(props) => (
-               <form onSubmit={props.handleSubmit}>
-                  <input
-                     type="text"
-                     onChange={props.handleChange}
-                     onBlur={props.handleBlur}
-                     value={props.values.name}
-                     name="name"
-                  />
-                  {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+         <div className={s.popupFromWrap}>
+            <span className={s.popupFormClose} onClick={() => setPopupMode(false, 'form')}></span>
+            <div className={s.popupFormTitle}>
+               <h3>Чтобы заказать звонок</h3>
+               <span>впишите свое имя и телефон:</span>
+            </div>
+            <Formik
+               initialValues={{
+                  name: '',
+                  phone: '',
+                  policy: false
+               }}
+               validationSchema={popupFormSchema}
+               onSubmit={(values, actions) => {
+                  alert(values.name, values.phone)
+               }}
+            >
+               {(props) => (
+                  <form onSubmit={props.handleSubmit}>
+                     <div className={s.wrap}>
+                        <input
+                           type="text"
+                           onChange={props.handleChange}                           
+                           value={props.values.name}
+                           name="name"
+                           placeholder="Ваше имя:"
+                           className={cn({[s.errorMsg]: props.errors.name},s.textInput)}
+                        />
+                     </div>
+                     <div className={s.wrap}>
+                        <InputMask mask="+7-999-9999999"
+                                   type="text"
+                                   name="phone"
+                                   onChange={props.handleChange}                                   
+                                   className={cn({[s.errorMsg]: props.errors.name},s.textInput)}
+                                   placeholder="Ваш телефон:"
+                                   value={props.values.phone}
+                        />
+                     </div>
+                     <div className={s.policyBlock}>
+                        <p className={s.checkInput}>
+                           <input
+                              id="checkBox"
+                              type="checkbox"
+                              onChange={props.handleChange}                              
+                              value={props.values.policy}
+                              name="policy"
+                           />
+                           <label htmlFor="checkBox">
+                              Я принимаю условия
+                           </label>
+                        </p>
 
-                  <InputMask mask="+7-999-999-99-99"
-                             type="text"
-                             name="phone"
-                             onChange={props.handleChange}
-                             onBlur={props.handleBlur}
-                             value={props.values.phone}
-                  />
-                  {props.errors.phone && <div id="feedback">{props.errors.phone}</div>}
+                        <div>
+                           <a href="#anch">политики конфиденциальности</a>
+                        </div>
+                     </div>
 
-
-                  <button type="submit" className={s.btnYellow} disabled={props.isSubmitting}>Заказать звонок</button>
-               </form>
-            )}
-         </Formik>
-
-
+                     <button type="submit"
+                             className={s.btnYellow}
+                             disabled={!props.values.policy}>
+                        Заказать звонок
+                     </button>
+                  </form>
+               )}
+            </Formik>
+         </div>
       </div>
    )
 }
+
+const mapStateToProps = (state) => {
+   return {}
+};
+
+export default connect(mapStateToProps, {setPopupMode})(PopupForm)
